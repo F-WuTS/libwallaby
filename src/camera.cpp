9e8a739f3bd798409ad4e755ca53c131a9e0f090
Rafael Schreiber <raf.schreiber@gmail.com>
2018-11-26 22:55:28 +0100

    Added compability for OpenCV4

--- src/camera.cpp
+++ src/camera.cpp
@@ -17,6 +17,9 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/types_c.h>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/videoio/legacy/constants_c.h>
 
 #ifndef NOT_A_WALLABY
 #include <errno.h>
